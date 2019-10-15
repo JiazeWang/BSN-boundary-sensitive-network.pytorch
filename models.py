6a06770e731783042e038be722f2432fa8ca5ed2
JiazeWang <jiazewang@csu.edu.cn>
2019-10-15 17:04:39 +0800

    1

--- models.py
+++ models.py
@@ -6,18 +6,18 @@ import torch.nn.functional as F
 import torch.nn as nn
 from torch.nn import init
 
-            
+
 class TEM(torch.nn.Module):
     def __init__(self, opt):
         super(TEM, self).__init__()
-        
+
         self.feat_dim = opt["tem_feat_dim"]
         self.temporal_dim = opt["temporal_scale"]
         self.batch_size= opt["tem_batch_size"]
         self.c_hidden = opt["tem_hidden_dim"]
         self.tem_best_loss = 10000000
-        self.output_dim = 3  
-        
+        self.output_dim = 3
+
         self.conv1 = torch.nn.Conv1d(in_channels=self.feat_dim,    out_channels=self.c_hidden,kernel_size=3,stride=1,padding=1,groups=1)
         self.conv2 = torch.nn.Conv1d(in_channels=self.c_hidden,out_channels=self.c_hidden,kernel_size=3,stride=1,padding=1,groups=1)
         self.conv3 = torch.nn.Conv1d(in_channels=self.c_hidden,out_channels=self.output_dim,   kernel_size=1,stride=1,padding=0)
@@ -34,16 +34,20 @@ class TEM(torch.nn.Module):
             self.weight_init(m)
 
     def forward(self, x):
+        print "input.shape:", x.shape()
         x = F.relu(self.conv1(x))
+        print "x1.shape:", x.shape()
         x = F.relu(self.conv2(x))
+        print "x2.shape:", x.shape()
         x = torch.sigmoid(0.01*self.conv3(x))
+        print "x.shape:", x.shape()
         return x
 
 class PEM(torch.nn.Module):
-    
+
     def __init__(self,opt):
         super(PEM, self).__init__()
-        
+
         self.feat_dim = opt["pem_feat_dim"]
         self.batch_size = opt["pem_batch_size"]
         self.hidden_dim = opt["pem_hidden_dim"]
@@ -51,7 +55,7 @@ class PEM(torch.nn.Module):
         self.u_ratio_l = opt["pem_u_ratio_l"]
         self.output_dim = 1
         self.pem_best_loss = 1000000
-        
+
         self.fc1 = torch.nn.Linear(in_features=self.feat_dim,out_features=self.hidden_dim,bias =True)
         self.fc2 = torch.nn.Linear(in_features=self.hidden_dim,out_features=self.output_dim,bias =True)
 
